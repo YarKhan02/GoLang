62b942b19f84f534d76931999569018691e490a1
YarKhan02 <yarkhan025@gmail.com>
2024-07-24 00:23:54 +0300

    implemented interface in go routines

--- goroutine-aggregating-data/main.go
+++ goroutine-aggregating-data/main.go
@@ -6,38 +6,60 @@ import (
 	"time"
 )
 
-func fetchUser() string {
-	time.Sleep(time.Millisecond * 100)
-
-	return "Wali Yar khan"
+type App interface {
+	fetchUser()
+	fetchLikes()
+	fetchMatch()
 }
 
-func fetchLikes(userName string, respch chan any, wg *sync.WaitGroup) {
+type data struct {
+	user_name string
+	likes     int
+	match     string
+}
+
+func CreateData(name string, likes int, match string) *data {
+	return &data{
+		user_name: name,
+		likes:     likes,
+		match:     match,
+	}
+}
+
+func (d *data) fetchUser() string {
+	time.Sleep(time.Millisecond * 100)
+
+	return d.user_name
+}
+
+func (d *data) fetchLikes(userName string, respch chan any, wg *sync.WaitGroup) {
 	time.Sleep(time.Millisecond * 150)
 
-	respch <- 11
+	respch <- d.likes
 	wg.Done()
 }
 
-func fetchMatch(userName string, respch chan any, wg *sync.WaitGroup) {
+func (d *data) fetchMatch(userName string, respch chan any, wg *sync.WaitGroup) {
 	time.Sleep(time.Millisecond * 100)
 
-	respch <- "Maryam"
+	respch <- d.match
 	wg.Done()
 }
 
 func main() {
 	start := time.Now()
 
-	userName := fetchUser()
+	person := CreateData("wali", 11, "maryam")
+
+	userName := person.fetchUser()
 
 	respch := make(chan any, 3)
 	wg := &sync.WaitGroup{}
 
 	wg.Add(2)
 
-	go fetchLikes(userName, respch, wg)
-	go fetchMatch(userName, respch, wg)
+	go person.fetchLikes(userName, respch, wg)
+	go person.fetchMatch(userName, respch, wg)
 
 	wg.Wait()
 	close(respch)
